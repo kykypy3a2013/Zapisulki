d38d945d2496d7e1c9a3501b90d48f919cb6deb6
Pavel <dunaevpavel1998@yandex.ru>
2026-01-21 09:04:53 +0400

    started

---
+++ app/src/main/java/com/example/zapisulki/ui/theme/Theme.kt
@@ -0,0 +1,23 @@
+package com.example.zapisulki.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = androidx.compose.ui.graphics.Color(0xFF6200EE),
+    secondary = androidx.compose.ui.graphics.Color(0xFF03DAC5),
+    tertiary = androidx.compose.ui.graphics.Color(0xFF3700B3)
+)
+
+@Composable
+fun MyAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        typography = Typography(),
+        content = content
+    )
+}
